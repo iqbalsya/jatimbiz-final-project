f832b3d2160cc47f979d050140f02ac8f0d87783
iqbalsya <muh.iqbalsya2@gmail.com>
2024-06-20 19:45:24 +0700

    memperbaiki sidebar dashboard

--- resources/views/components/dashboard/navbars/sidebar.blade.php
+++ resources/views/components/dashboard/navbars/sidebar.blade.php
@@ -6,7 +6,7 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand d-flex text-wrap align-items-center" href=" {{ route('dashboard') }} ">
+        <a class="navbar-brand d-flex text-wrap align-items-center" href="">
             <span class=" h6 pt-1 font-weight-bold text-white">Dashboard Jatim<span class="fw-normal">BIZ</span></span>
         </a>
     </div>
